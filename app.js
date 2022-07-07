ffa3de7998486f0a749f68b610ed46836eae3015
Ahmed Mustafa <ahmed.mustafa@itworx.com>
2022-07-07 10:14:49 +0200

    add middlewares

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const morgan = require('morgan')
 const debug = require('debug')('app');
 
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.listen(PORT, () => {
   debug(`listening on port ${chalk.green(PORT)}`);
