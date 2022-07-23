491908a93ecc92412e190a5bf68b5049602f9019
Ahmed Mustafa <ahmed.mustafa@itworx.com>
2022-07-23 09:31:39 +0200

    adding html and css files with basic configurations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require('morgan')
 const debug = require('debug')('app');
+const path = require('path')
 
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,17 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/home', (req, res) => {
+  res.render('index')
+})
+
 app.listen(PORT, () => {
-  debug(`listening on port ${chalk.green(PORT)}`);
+  debug(`listening on port ${PORT}`);
 });
