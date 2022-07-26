a3b8dcc217e7b54e3cdae8a43db6e5ff8568419b
Ahmed Mustafa <ahmed.mustafa@itworx.com>
2022-07-26 03:04:12 +0200

    add auth module

---
+++ src/routes/authRouter.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const debug = require("debug")("app:authRouter");
+const passport = require("passport");
+const User = require("../model/User");
+
+const authRouter = express.Router();
+
+authRouter
+  .route("/register")
+  .post(async (req, res) => {
+    const { firstName, lastName, username, password } = req.body;
+
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const userObj = new User({
+        firstName,
+        lastName,
+        username,
+        password: hashedPassword,
+      });
+      const user = await userObj.save();
+
+      req.logIn(user, (err) => {
+        res.redirect("/");
+      });
+    } catch (error) {
+      debug(error);
+    }
+
+  });
+
+authRouter
+  .route("/login")
+  .post(
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/products",
+    })
+  );
+
+  authRouter.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+module.exports = authRouter;
